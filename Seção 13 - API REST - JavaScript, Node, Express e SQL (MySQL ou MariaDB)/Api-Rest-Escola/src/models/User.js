14eddd11f633cb747ddec1df89c1b65cf2622c2b
williansoncini <willian_soncini@hotmail.com>
2022-03-07 20:55:02 -0300

    Até a aula 177 - jwt token

--- Seção 13 - API REST - JavaScript, Node, Express e SQL (MySQL ou MariaDB)/Api-Rest-Escola/src/models/User.js
+++ Seção 13 - API REST - JavaScript, Node, Express e SQL (MySQL ou MariaDB)/Api-Rest-Escola/src/models/User.js
@@ -1,5 +1,6 @@
 import Sequelize, { Model } from 'sequelize';
 import bcryptjs from 'bcryptjs';
+import { password } from '../config/database';
 
 export default class User extends Model {
   static init(sequelize) {
@@ -51,5 +52,9 @@ export default class User extends Model {
     });
 
     return this;
+  }
+
+  passwordIsValid(password) {
+    return bcryptjs.compare(password, this.password_hash);
   }
 }
--- Seção 13 - API REST - JavaScript, Node, Express e SQL (MySQL ou MariaDB)/api_rest/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const port = 3001;
-
-app.listen(port, () => {
-  console.log('O pai está on!');
-  console.log(`http://localhost:${port}`);
-});
