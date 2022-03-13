95eeebfd76afadf8c8eab05abebb91a5bec288a0
williansoncini <willian_soncini@hotmail.com>
2022-03-13 10:45:45 -0300

    208 - Colocando o history - react-base

--- Seção 15  - React - Redux - Redux Saga - React Hooks e componentes funcionais - Avançado/react-base/src/App.js
+++ Seção 15  - React - Redux - Redux Saga - React Hooks e componentes funcionais - Avançado/react-base/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 import RoutesApp from './routes';
+import history from './services/history';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <Header />
       <RoutesApp />
       <GlobalStyles />
-    </BrowserRouter>
+    </Router>
   );
 }
 
