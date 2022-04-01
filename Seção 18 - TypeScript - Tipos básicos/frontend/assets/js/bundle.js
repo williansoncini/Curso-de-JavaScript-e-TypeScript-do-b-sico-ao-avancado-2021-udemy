2bc48ae94b45396487376d2c8fda23ba610dfb21
williansoncini <willian_soncini@hotmail.com>
2022-04-01 00:14:54 -0300

    Subindo seção 18 - Inicio

---
+++ Seção 18 - TypeScript - Tipos básicos/frontend/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+console.log('teste');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
